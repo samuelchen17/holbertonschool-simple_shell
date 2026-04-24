193ae16ae89b6b10ddf5e46f1a7c3f9f872d460f
Samuel Chen <sche842@gmail.com>
2026-04-24 18:30:23 +1000

    get tokens function implementation

--- main.c
+++ main.c
@@ -1,5 +1,36 @@
 #include "shell.h"
 
+char **get_tokens(char *line)
+{
+char **args_arr;
+char *token, *tmp = strdup(line);
+int i = 0, count = 0;
+
+/* first iteration count tokens for malloc size*/
+token = strtok(tmp, " ");
+while (token != NULL)
+{
+count++;
+/* strtok stores /0 when it stops, by using NULL it will continue to next */
+token = strtok(NULL, " ");
+}
+free(tmp);
+
+/* allocate exact mem */
+args_arr = malloc((count + 1) * sizeof(char*));
+
+/* loop to store tokens */
+token = strtok(line, " ");
+while (token != NULL)
+{
+args_arr[i] = strdup(token);
+token = strtok(NULL, " ");
+i++;
+}
+args_arr[i] = NULL;
+
+return (args_arr);
+}
 
 void get_prompt(void)
 {
@@ -12,16 +43,17 @@ while (1)
 printf("$ ");
 input = getline(&line, &n, stdin);
 
+/* break loop upon EOF or error*/
 if (input == -1)
 {
 printf("\n");
 break;
 }
-else
-printf("%s", line);
 
+printf("%s", line);
 }
-free (line);
+
+free(line);
 }
 
 int main(void)
