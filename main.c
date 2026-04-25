a1a6e8f8e0105b93690c1d3bada0c746da3e0091
bz888 <ziyu.ben158@gmail.com>
2026-04-25 13:02:51 +1000

    format main.c with indents

--- main.c
+++ main.c
@@ -1,162 +1,132 @@
 #include "shell.h"
 
 /**
- * free_args_arr - free argument array created from tokenization
- * @args_arr: argument array created from tokenization
+ * free_args_arr - Free argument array created from tokenization
+ * @args_arr: Argument array created from tokenization
  */
 void free_args_arr(char **args_arr)
 {
-int i = 0;
+	int i = 0;
 
-if (!args_arr)
-return;
+	if (!args_arr)
+		return;
 
-while (args_arr[i] != NULL)
-{
-free(args_arr[i]);
-i++;
-}
-free(args_arr);
+	while (args_arr[i] != NULL)
+	{
+		free(args_arr[i]);
+		i++;
+	}
+	free(args_arr);
 }
 
 /**
- * fork_and_execve - clone current process and execute command in child
- * @args_arr: argument array created from tokenization
+ * fork_and_execve - Clone current process and execute command in child
+ * @args_arr: Argument array created from tokenization
  */
 void fork_and_execve(char **args_arr)
 {
-pid_t pid;
-int status;
+	pid_t pid;
+	int status;
 
-pid = fork();
-if (pid == -1)
-{
-perror("fork failed");
-free_args_arr(args_arr);
-exit(1);
-}
-/* on child creation success */
-if (pid == 0)
-{
-/*
-* for testing
-* printf("token passed: [%s]\n", args_arr[0]);
-*/
-execve(args_arr[0], args_arr, NULL);
-perror("execve failed");
-free_args_arr(args_arr);
-exit(1);
-}
-else
-{
-/* parent to wait for child process to exit */
-wait(&status);
-free_args_arr(args_arr);
-}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork failed");
+		free_args_arr(args_arr);
+		exit(1);
+	}
+
+	if (pid == 0)
+	{
+		execve(args_arr[0], args_arr, NULL);
+		perror("execve failed");
+		free_args_arr(args_arr);
+		exit(1);
+	}
+
+	wait(&status);
+	free_args_arr(args_arr);
 }
 
 /**
- * get_tokens - separate words from user input into tokens
- * @line: string of user input
+ * get_tokens - Separate words from user input into tokens
+ * @line: String of user input
  *
- * Return: array of arguments
+ * Return: Array of arguments
  */
-
 char **get_tokens(char *line)
 {
-char **args_arr;
-/* tmp needed as strtok modifies the str */
-char *token, *tmp = strdup(line);
-int i = 0, count = 0;
+	char **args_arr;
+	char *token, *tmp = strdup(line);
+	int i = 0, count = 0;
 
-/* first iteration count tokens for malloc size*/
-token = strtok(tmp, " ");
-while (token != NULL)
-{
-count++;
-/* strtok stores /0 when it stops, by using NULL it will continue to next */
-token = strtok(NULL, " ");
-}
-free(tmp);
+	token = strtok(tmp, " ");
+	while (token != NULL)
+	{
+		count++;
+		token = strtok(NULL, " ");
+	}
+	free(tmp);
 
-/* allocate exact memory */
-args_arr = malloc((count + 1) * sizeof(char *));
+	args_arr = malloc((count + 1) * sizeof(char *));
+	if (args_arr == NULL)
+		return (NULL);
 
-/* loop to store tokens */
-token = strtok(line, " ");
-while (token != NULL)
-{
-args_arr[i] = strdup(token);
-token = strtok(NULL, " ");
-i++;
-}
-args_arr[i] = NULL;
+	token = strtok(line, " ");
+	while (token != NULL)
+	{
+		args_arr[i] = strdup(token);
+		token = strtok(NULL, " ");
+		i++;
+	}
+	args_arr[i] = NULL;
 
-/*
-* for testing
-i = 0;
-while (args_arr[i] != NULL)
-{
-printf("args arr[%i]: %s\n", i, args_arr[i]);
-i++;
-}
-*/
-
-return (args_arr);
+	return (args_arr);
 }
 
 /**
- * get_prompt - get user input
+ * get_prompt - Get user input
  */
-
 void get_prompt(void)
 {
-char *line = NULL;
-size_t n = 0;
-ssize_t input;
-char **args_arr;
+	char *line = NULL;
+	size_t n = 0;
+	ssize_t input;
+	char **args_arr;
 
-while (1)
-{
-if (isatty(STDIN_FILENO))
-{
-printf("$ ");
-}
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
 
-input = getline(&line, &n, stdin);
+		input = getline(&line, &n, stdin);
+		if (input == -1)
+			break;
 
-/* break loop upon EOF or error*/
-if (input == -1)
-{
-break;
-}
+		line[strcspn(line, "\n")] = '\0';
+		args_arr = get_tokens(line);
 
-/* remove \n for tokenization */
-line[strcspn(line, "\n")] = '\0';
+		if (args_arr == NULL || args_arr[0] == NULL)
+		{
+			free_args_arr(args_arr);
+			continue;
+		}
 
-args_arr = get_tokens(line);
+		fork_and_execve(args_arr);
+	}
 
-/* block invalid command before execve */
-if (args_arr[0] == NULL)
-{
-free_args_arr(args_arr);
-continue;
-}
-
-fork_and_execve(args_arr);
-}
-
-free(line);
+	free(line);
 }
 
 /**
- * main - entry to program
- * Return: int
+ * main - Entry point
+ *
+ * Return: Always 0
  */
-
 int main(void)
 {
-get_prompt();
+	get_prompt();
 
-return (0);
+	return (0);
 }
+
